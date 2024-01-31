601370ad13c10f393e44f5dc5214a9d3f9c03160
M. Abdullah Bin Aftab <heighter79@gmail.com>
2024-02-01 03:23:02 +0500

    day 1, adding some core functionalities

--- main.py
+++ main.py
@@ -13,18 +13,6 @@ from sqlalchemy.orm import relationship
 # Import your forms from the forms.py
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
-'''
-Make sure the required packages are installed: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from the requirements.txt for this project.
-'''
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
